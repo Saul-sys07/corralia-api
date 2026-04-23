a09753abe9a000ca8e9ff6b9db2db7d9004fc8df
Saúl-sys07 <zaul365@gmail.com>
2026-04-22 23:24:42 -0600

    fix cors para vercel

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="Corralia API v4")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://corralia.vercel.app"],
+    allow_origins=["http://localhost:5173", "https://corralia-react-h0e10gno8-saul-sys07s-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
